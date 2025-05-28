5965fdca580b621d581943a8908d3578aae4cc11
Concave Trillion <ConcaveTrillion@gmail.com>
2025-05-28 14:27:06 -0400

    more training data

--- dev-env-setup.sh
+++ dev-env-setup.sh
@@ -8,10 +8,15 @@ python -m pip install --upgrade pip
 
 # need the full doctr package, not included with pip install
 
-git clone https://github.com/mindee/doctr.git doctr_package
-cd doctr_package
+cd ..
+git clone https://github.com/mindee/doctr.git doctr
+cd doctr
 pip install -e .[torch]
+cd ..
+cd train_pgdp_ocr
 
 # use pd_book_tools from local install as well
 
 pip install -e ../pd_book_tools
+
+pip install -r requirements.txt
